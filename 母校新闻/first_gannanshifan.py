037392243e96c615a4bfd97ee9304cd2028d17c5
lion with  rabbit <1820245610@qq.com>
2018-09-12 12:00:50 +0800

    更改目录文件

--- 母校新闻/first_gannanshifan.py
+++ 母校新闻/first_gannanshifan.py
@@ -1,12 +1,16 @@
 """
 母校新闻小爬虫
+母校网站"赣南师范大学http://www.gnnu.cn"
 """
 import requests
 from lxml import etree
 
 
 def xinxi_list():
-
+    """
+    分析网页：进行新闻网页分析，获取爬虫信息
+    :return: 路由列表
+    """
     url_list = []
     url_get = "http://www.gnnu.cn/html/63/"
     url_list.append(url_get)
@@ -17,7 +21,11 @@ def xinxi_list():
 
 
 def get_content(url_get):
-
+    """
+    获取的函数文件
+    :param url_get: 参数
+    :return: 返回新闻列表
+    """
     response = requests.get(url=url_get)
     content = response.content.decode("gb18030")
     content = etree.HTML(content)
@@ -27,6 +35,11 @@ def get_content(url_get):
 
 
 def url_get(url_list):
+    """
+
+    :param url_list: 文件写入
+    :return: none
+    """
     for i in url_list:
         print("working in %s" % i)
         f_title = get_content(i)
